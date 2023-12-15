7543c6d5daaa0a97faa2f6f0b1d05ce23bb32071
DmitriyVladimirovich <150802655+DmitriyVladimirovich@users.noreply.github.com>
2023-12-15 15:43:53 +0300

    Add files via upload

---
+++ Task11_2sc.sql
@@ -0,0 +1,11 @@
+drop database if exists task11_2;
+create database task11_2;
+use task11_2;
+
+create table `Почтовые отделения` (id int primary key auto_increment, o_addr varchar(250), t_work varchar(100));
+
+create table `Работники` (id int primary key, f_name varchar(50), l_name varchar(50), m_name varchar(50), num_p int, ser_p varchar(10), giv_h varchar(1000),
+						  l_addr varchar(250), tk_num int, po int, foreign key (po) references `Почтовые отделения`(id), index (num_p, ser_p));
+
+create table `Посылки` (id int primary key, weight smallint, cat tinyint, track int, 
+						from_ int, where_ int, foreign key (from_) references `Почтовые отделения`(id), foreign key (where_) references `Почтовые отделения`(id));
